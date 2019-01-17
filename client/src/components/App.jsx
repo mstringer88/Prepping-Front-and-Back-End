cdc0725c4bc5462ced2ceec398e4764d26fe2df4
Michael Stringer <mstringer09@yahoo.com>
2019-01-17 11:06:35 -0600

    added new paths for new files

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,9 +1,15 @@
 import React, { Component, Fragment } from 'react';
 import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
+import PrivateRoute from './auth/privateRoute';
 import AllChirps from './AllChirps';
 import DisplayChirp from './DisplayChirp';
 import SingleChirp from './SingleChirp';
 import EditChirp from './EditChirp';
+import Home from './Home';
+import CreateLogin from './CreateLogin';
+import Login from './auth/login';
+import Logout from './auth/logout';
+import WriteChirp from './WriteChirp';
 
 class Navigation extends Component {
 
@@ -12,9 +18,14 @@ class Navigation extends Component {
             <Router>
                 <Fragment>
                     <Switch>
-                        <Route exact path="/" component={AllChirps} />
+                        <Route exact path="/" component={Home} />
+                        <Route exact path="/login" component={Login} />
+                        <Route exact path="/logout" component={Logout} />
+                        <PrivateRoute exact path="/createchirp" component={WriteChirp} />
+                        <Route exact path="/register" component={CreateLogin} />
+                        <Route exact path="/allchirps" component={AllChirps} />
                         <Route exact path="/chirp/:id" component={SingleChirp} />
-                        <Route exact path="/chirp/:id/edit" component={EditChirp} />
+                        <PrivateRoute exact path="/chirp/:id/edit" component={EditChirp} />
                     </Switch>
                 </Fragment>
             </Router>
