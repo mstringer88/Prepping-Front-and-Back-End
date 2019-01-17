820cda883690d2c639396bd9150db1f68f87f6bb
Michael Stringer <mstringer09@yahoo.com>
2019-01-17 11:13:04 -0600

    added new api routes and authentication

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -1,9 +1,23 @@
 import { Router } from 'express';
 import peopleRouter from './people';
 import chirpsRouter from './chirps';
+import usersRouter from './user';
+import authRouter from './auth';
+import { isLoggedIn, tokenMiddleware } from '../middleware/auth.mw';
+import authorRouter from './authors';
 
 let router = Router();
 
+
+router.use('/auth', authRouter);
+router.use('/authors', authorRouter);
+
+router.route('*')
+    .post(tokenMiddleware, isLoggedIn)
+    .put(tokenMiddleware, isLoggedIn)
+    .delete(tokenMiddleware, isLoggedIn);
+
+router.use('/users', usersRouter);    
 router.use('/people', peopleRouter);
 router.use('/chirps', chirpsRouter)
 
