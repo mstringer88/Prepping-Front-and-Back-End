487a679957e556b84a724c849ab135f475a0af69
Michael Stringer <mstringer09@yahoo.com>
2019-01-17 11:08:22 -0600

    changed info to be displayed and formatting

--- client/src/components/SingleChirp.jsx
+++ client/src/components/SingleChirp.jsx
@@ -1,46 +1,45 @@
 import React, { Component, Fragment } from 'react';
 import { Link } from 'react-router-dom';
+import * as chirpServices from '../services/chirps';
 
 class SingleChirp extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            postObject: {}
+            singleChirp: {}
         }
     }
 
-    componentDidMount() {
-        fetch(`/api/chirps/${this.props.match.params.id}`)
-            .then(res => res.json())
-            .then(data => this.setState({
-                postObject: data
-            }))
+    componentWillMount() {
+        chirpServices.one(this.props.match.params.id)
+            .then(chirp => {
+                this.setState({
+                    singleChirp: chirp
+                });
+            });
     }
 
     handleDeleteClick(e) {
-        fetch(`/api/chirps/${this.props.match.params.id}`, {
-            method: 'DELETE'
-        })
-            .catch(err => (console.log(`I'm not deleted: ${err}`)))
+        chirpServices.destroy(this.props.match.params.id)
+            .catch(err => (console.log(`Not deleted: ${err}`)))
     }
 
     render() {
+        const { id, title, text, location } = this.state.singleChirp;
         return (
             <Fragment>
-                <div className="container">
-                    <div className="card">
-                        <div className="card-header">
-                            Display Chirp Info
+                <article className="card mb-4">
+                    <header className="card-header text-center">
+                        <h1 className="card-title">{title}</h1>
+                    </header>
+                    <div className="card-body">
+                        <p>{text}</p>
+                        <p>{location}</p>
                     </div>
-                        <div className="card-body">
-                            <h1 className="card-title">{this.state.postObject.Name}</h1>
-                            <p className="card-text">{this.state.postObject.Chirp}</p>
-                            <Link to={`/chirp/${this.props.match.params.id}/edit`}>Edit Chirp</Link>
-                        </div>
-                    </div>
-                </div>
-                <div className="container">
-                    <button onClick={(e) => { this.handleDeleteClick(e); this.props.history.push('/') }} className="btn btn-danger" data-toggle="tooltip" data-placement="top" title="Delete Chirp">X</button>
+                </article>
+                <div>
+                    <Link to={`/chirp/${this.props.match.params.id}/edit`}>Edit Chirp </Link>
+                    <button onClick={(e) => { this.handleDeleteClick(e); this.props.history.push('/allchirps') }} className="btn btn-danger" data-toggle="tooltip" data-placement="top" title="Delete Chirp">X</button>
                 </div>
             </Fragment>
         )
